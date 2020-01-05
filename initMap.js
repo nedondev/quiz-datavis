8bdcd1f07c86a236b7cbedc84746e46532900fd0
kanisornpitakwong <49259723+kanisornpitakwong@users.noreply.github.com>
2020-01-05 11:35:47 +0700

    Update centering map

--- initMap.js
+++ initMap.js
@@ -1,5 +1,5 @@
 function initMap(LAT, LON) {
-  var map = L.map('mapid').setView([51.505, -0.09], 13);
+  var map = L.map('mapid').setView([LAT, LON], 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
