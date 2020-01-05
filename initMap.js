8bd1902fb900edfeaac23a78da61c0649f888e5b
kanisornpitakwong <49259723+kanisornpitakwong@users.noreply.github.com>
2020-01-05 15:41:44 +0700

    Add update gauge and map from bar

--- initMap.js
+++ initMap.js
@@ -1,11 +1,20 @@
-function initMap(LAT, LON) {
+function initMap() {
   var map = L.map('mapid').setView([LAT, LON], 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+  return map
+}
+
+function addMarker(lat, lon) {
   L.marker([LAT, LON]).addTo(map)
     .bindPopup('Data collected from here.')
     .openPopup();
 }
+function redrawMarker(LAT, LON) {
+  
+}
+
+
